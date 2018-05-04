0891b9523ff439f8d6aa65f84176c9bced3b90c3
Flavio Farias <flavio.farias@ccc.ufcg.edu.br>
2018-05-04 11:05:42 -0300

    R01-01 R02-02

---
+++ R01-01-Rot-SimpleSorting-Simultaneous-Bubble-environment/src/main/java/sorting/simpleSorting/InsertionSort.java
@@ -0,0 +1,27 @@
+package sorting.simpleSorting;
+
+import sorting.AbstractSorting;
+
+/**
+ * As the insertion sort algorithm iterates over the array, it makes the
+ * assumption that the visited positions are already sorted in ascending order,
+ * which means it only needs to find the right position for the current element
+ * and insert it there.
+ */
+public class InsertionSort<T extends Comparable<T>> extends AbstractSorting<T> {
+
+	@Override
+	public void sort(T[] array, int leftIndex, int rightIndex) {
+		for (int i = 1; i <= rightIndex; i++) {
+
+			T key = array[i];
+			int j = i;
+
+			while ((j > leftIndex) && (array[j - 1].compareTo(key) > 0)) {
+				array[j] = array[j - 1];
+				j--;
+			}
+			array[j] = key;
+		}
+	}
+}
